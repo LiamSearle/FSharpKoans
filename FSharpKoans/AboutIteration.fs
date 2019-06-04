39bb93dff3e5ca1f7bc2b8d2c79f7e9cbb28ff03
Liam Searle <liamsearle7@gmail.com>
2019-06-04 15:58:54 +0200

    sdfghjk

--- FSharpKoans/AboutIteration.fs
+++ FSharpKoans/AboutIteration.fs
@@ -16,18 +16,18 @@ module ``05: To iterate is human; to recurse, divine`` =
                 | true -> converge (d+10) c (n+1)
                 | false -> converge (d - 1) c (n+1)
             | true -> n
-        converge 3 10  |> should equal __
+        converge 3 10 0 |> should equal 4
 
     [<Test>]
     let ``02 Tail recursion stops a stack overflow from occurring`` () =
         // CHANGE the recursive function to be tail recursive.
         let myfun n =
-            let sq = n*n
-            let v = sq*sq*sq*sq
-            let rec inner count =
-                match count = v with
-                | true -> 0
-                | false -> -1 + inner (count+1)
-            inner sq
+            let sq = n*n //sq = 144
+            let v = sq*sq*sq*sq // v = 429 000 000
+            let rec inner count fuck = //count = 144 || fuck = 1
+                match count = v with // is count (144) = v (429 000 000)
+                | true -> 0 // base case
+                | false -> inner (count+1) (count-fuck) // inner (145) (143)
+            inner sq 1
 
-        myfun 12 |> should equal
+        myfun 12 |> should equal 0
